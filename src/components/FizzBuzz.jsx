ea7fc9e000001b3c1e3cc2a8ac13c1eb52d23ce4
Diego Chacón <dchacon130@gmail.com>
2023-09-20 11:18:19 -0500

    Last commit, with better functions

--- src/components/FizzBuzz.jsx
+++ src/components/FizzBuzz.jsx
@@ -1,47 +1,50 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
 import { show_alert } from './functions/functions';
 
 const FizzBuzz = () => {
-    const [inputRangeA, setInputRangeA] = useState('');
-    const [inputRangeB, setInputRangeB] = useState('');
+    const [data, setData] = React.useState([]);
 
-    const [data, setData] = useState([]); // Estado para almacenar los datos de la API
-    // Función para cargar los datos de la API usando Axios
-    const fetchData = async () => {
-        try {
-            if (inputRangeA === '' && inputRangeB === '') {
+    const [inputRangeA, setInputRangeA] = React.useState('');
+    const [inputRangeB, setInputRangeB] = React.useState('');
+
+    React.useEffect(() => {
+        const fetchData = async () => {
+            try {
                 const response = await axios.get('http://127.0.0.1:8000/fizzbuzz');
                 setData(response.data);
+            } catch (error) {
+                console.error('Error al cargar los datos:', error);
             }
-            else if (inputRangeA === inputRangeB) {
+        }
+        fetchData();
+    }, [inputRangeA, inputRangeB]);
+
+    const buttonByRange = async () => {
+        try {
+            if (inputRangeA === inputRangeB) {
                 show_alert('Los numeros no deben ser iguales...', 'warning');
             } else {
-                const url = 'http://127.0.0.1:8000/fizzbuzz/' + inputRangeA.toString() + '/' + inputRangeB.toString();
+                const url = `http://127.0.0.1:8000/fizzbuzz/${inputRangeA}/${inputRangeB}`;
                 const response = await axios.get(url);
                 setData(response.data);
             }
-
         } catch (error) {
             console.error('Error al cargar los datos:', error);
         }
     };
-    // Cargar los datos de la API cuando el componente se monte
-    useEffect(() => {
-        fetchData();
-    }, []); // El segundo argumento vacío asegura que esto se ejecute solo una vez al montar el componente
 
-    const [inputNumber, setInputNumber] = useState('');
-    const [result, setResult] = useState([]);
+    const [result, setResult] = React.useState([]);
+    const [inputNumber, setInputNumber] = React.useState('');
 
     const handleButtonClick = async () => {
         try {
             if (inputNumber.trim() === '') {
                 show_alert('Completa el campo numero', 'warning');
             } else {
-                const url = 'http://127.0.0.1:8000/fizzbuzz/' + inputNumber.toString();
+                const url = `http://127.0.0.1:8000/fizzbuzz/${inputNumber}`;
                 const response = await axios.get(url);
                 setResult(response.data);
             }
@@ -51,10 +54,10 @@ const FizzBuzz = () => {
     };
 
     return (
-        <div className="App" data-bs-theme="light">
-            <div className="container mt-5">
+        <div className="App">
+            <div className="container">
                 <div className='row'>
-                    <div className='col-10'>
+                    <div className='text-center pt-5 pb-5 col-10'>
                         <h1>EL FAMOSO FIZZ BUZZ</h1>
                     </div>
                     <div className='col-4'>
@@ -73,11 +76,6 @@ const FizzBuzz = () => {
                             <h4 key={value.item_id}>Resultado: {value.item_id} | {value.message}</h4>
 
                         ))}
-                        <hr></hr>
-                        <h3>Consulta por rango de numeros:</h3>
-                        <input type='number' value={inputRangeA} onChange={(e) => setInputRangeA(e.target.value)} />
-                        <input type='number' value={inputRangeB} onChange={(e) => setInputRangeB(e.target.value)} />
-                        <button onClick={fetchData}>Calcular</button><br></br>
                         <hr></hr>
                         <p>El contenido de este proyecto lo puedes encontrar en GitHub:</p>
                         <div className='row'>
@@ -101,18 +99,22 @@ const FizzBuzz = () => {
                             </div>
                         </div>
                     </div>
-                    <div className='col-6'>
-                        <h1>Resultado</h1>
+                    <div className='col-8 text-center'>
+                        <h3>Consulta por rango de numeros:</h3>
+                        <input type='number' value={inputRangeA} onChange={(e) => setInputRangeA(e.target.value)} />
+                        <input type='number' value={inputRangeB} onChange={(e) => setInputRangeB(e.target.value)} />
+                        <button onClick={buttonByRange}>Calcular</button><br></br>
+                        <h1 className='pt-5'>Resultado</h1>
                         <p>Este es el resultado de la famosa prueba de Fizz Buzz.</p>
-                        <p>Pero no nos quedamos solo con un print(), tenemos más consultas en las otras dos pestañas.</p>
-                        <table className="table">
+                        <p>Pero no nos quedamos solo con un print(), creamos una API para ver los resultados.</p>
+                        <table className="table table-sm table-dark">
                             <thead>
                                 <tr>
                                     <th scope="col">#</th>
                                     <th scope="col">Resultado</th>
                                 </tr>
                             </thead>
-                            <tbody>
+                            <tbody className='table table-dark table-sm'>
                                 {data.map((item) => (
                                     <tr key={item.item_id}>
                                         <td>{item.item_id}</td>
@@ -121,15 +123,6 @@ const FizzBuzz = () => {
                                 ))}
                             </tbody>
                         </table>
-                        <nav aria-label="Page navigation">
-                            <ul className="pagination">
-                                <li className="page-item"><a className="page-link" href="#">Anterior</a></li>
-                                <li className="page-item"><a className="page-link" href="#">1</a></li>
-                                <li className="page-item"><a className="page-link" href="#">2</a></li>
-                                <li className="page-item"><a className="page-link" href="#">3</a></li>
-                                <li className="page-item"><a className="page-link" href="#">Siguiente</a></li>
-                            </ul>
-                        </nav>
                     </div>
                 </div>
             </div>
