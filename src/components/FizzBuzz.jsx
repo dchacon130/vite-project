fb660ebed9dc867f9b681f485e8c4b4240ed63db
Diego Chacón <dchacon130@gmail.com>
2023-09-20 11:24:05 -0500

    delete useState import

--- src/components/FizzBuzz.jsx
+++ src/components/FizzBuzz.jsx
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import axios from 'axios';
-import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
 import { show_alert } from './functions/functions';
 
 const FizzBuzz = () => {
